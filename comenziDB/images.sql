b6b2081cbdc25662efd586c67aa1f1d274475595
Sebastian Rabei <sebastianrabei@gmail.com>
2022-05-24 15:11:17 +0300

    db v2

---
+++ comenziDB/images.sql
@@ -0,0 +1,4 @@
+INSERT INTO musicland.images (image) VALUES
+	 ('http://img.picturequotes.com/2/540/539591/music-quote-1.jpg'),
+	 ('https://images.fineartamerica.com/images/artworkimages/mediumlarge/3/blue-electric-guitar-ian-mitchell.jpg'),
+	 ('https://www.superprof.co.in/blog/wp-content/uploads/2018/07/drums-600x400.jpg');
